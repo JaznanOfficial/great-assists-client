9f56a446fa20f7efc9bc5ebc749cfc7df9ee3f81
JaznanOfficial <jaznanofficial@gmail.com>
2022-11-20 17:19:12 +0600

    homepage done

--- src/Components/HomePage/WelcomeCarousel.js
+++ src/Components/HomePage/WelcomeCarousel.js
@@ -10,7 +10,7 @@ const WelcomeCarousel = () => {
                 <div className="container mx-auto">
                     <h1 className="md:text-3xl text-2xl font-bold my-5">
                         Welcome to{" "}
-                        <span className="bg-black px-1 rounded-lg">
+                        <span className="bg-black px-3 py-2 rounded-lg">
                             <span className="text-primary">Great</span>{" "}
                             <span className="text-white">Assists</span>
                         </span>
