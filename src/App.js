3cd1bef2eac5a93d7ed7cd0d683833a068b41cbe
JaznanOfficial <jaznanofficial@gmail.com>
2022-11-20 10:05:51 +0600

    volunteer done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './Pages/HomePage';
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import ScrollToTop from "react-scroll-to-top";
 
 function App() {
-  return (
-    <div className="App font-open">
-      <HomePage/>
-    </div>
-  );
+    return (
+        <div className="App font-open">
+            <ScrollToTop
+                smooth
+                color="#FE1A00"
+                viewBox="0 0 150 250"
+                style={{ borderRadius: "20%", right: 30, bottom: 100, }}
+            />
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
