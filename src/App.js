3eb658d84d46b7ce14a74cc4ec42b460eae627e3
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-29 22:30:29 +0600

    messenger chat add

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
                 viewBox="0 0 150 250"
                 style={{ borderRadius: "20%", right: 30, bottom: 100 }}
             />
-            <MessengerCustomerChat pageId="<PAGE_ID>" appId="<APP_ID>" />,
+            <MessengerCustomerChat pageId="100088200374139" appId="594155188895748" />,
             <Navigation />
             <Routes>
                 <Route path="/" element={<HomePage />}></Route>
